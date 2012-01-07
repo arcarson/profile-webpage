11c862c6ba171dcf8060022306bf955b16b39126
andrew carson <andrewcarson001@gmail.com>
2012-01-08 09:26:18 +1100

    2nd commit

--- js/custom.js
+++ js/custom.js
@@ -2,39 +2,34 @@ $(document).ready(function() {
   
   $(".selected").hover(
   function() {
-  $(this).stop().animate({"opacity": "1"}, "slow");
+    $(this).stop().animate({"opacity": "1"}, "slow")
   },
   function() {
-  $(this).stop().animate({"opacity": "0"}, "normal");
+    $(this).stop().animate({"opacity": "0"}, "normal")
   });
   
-
+  // $('#mobile-window p').css({'margin-left' : '-98px'});
   
-  // $('.content-tab').hide();
-  // 
-  // $('.nav-link a').click(function() {
-  //   if ($('.content-tab').filter(this.hash).is(':visible')){
-  //     
-  //   }
-  //   else {
-  //     $('.content-tab').slideUp();
-  //   }
-  //       
-  //   $('.content-tab').filter(this.hash).slideDown();
-  //   return false;
-  // });
+  $('#contact :first-child a').toggle(
+    function() {
+     $('#mobile-window p').animate({ left: '+=98'})
+     return false
+    },
+    function() {
+      $('#mobile-window p').animate({ left: '-=98'})
+      return false
+    });
    
    
-   $('#work').toggle(function() {
-     $('.column-1').animate({ right: '+=930'}, 1000, "swing");
-     $('.column-2').animate({ right: '+=1430'}, 1000, "swing");
-     $('.column-3').animate({ right: '+=1980'}, 1000, "swing");
-   },
-   function() {
-     $('.column-1').animate({ right: '-=930'}, 1000, "swing");
-     $('.column-2').animate({ right: '-=1430'}, 1000, "swing");
-     $('.column-3').animate({ right: '-=1980'}, 1000, "swing");
-   });
-   
-    
+  $('#main-content .project-tab').hover(
+    function() {
+      $(this).stop().animate({ backgroundColor: '#00b0d4'})
+      $(this).children('h2').stop().animate({color: '#fff'})
+      $(this).children('img').stop().animate({borderColor: '#23a1bb'})
+    }, 
+    function() {
+      $(this).stop().animate({ backgroundColor: '#f2f2f2'})
+      $(this).children('h2').stop().animate({color: '#00b0d4'})
+      $(this).children('img').stop().animate({borderColor: '#bbbbbb'})
+    });
 });
