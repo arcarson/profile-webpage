2c1e24cde42b8d4c1e0a142967481b61140690d8
andrew carson <andrewcarson001@gmail.com>
2012-02-29 21:31:54 +1100

    updated project pages to boilerplate template

--- js/script.js
+++ js/script.js
@@ -2,46 +2,26 @@
 
 */
 
-$(document).ready(function() {
+$(function() {
   
-  $(".selected").hover(
-  function() {
-    $(this).stop().animate({"opacity": "1"}, "slow")
-  },
-  function() {
-    $(this).stop().animate({"opacity": "0"}, "normal")
+  $('aside ul li').hover(function() {
+    $(this).stop().animate({'width': '293'}, 'fast')
+  },function() {
+    $(this).stop().animate({'width': '56'}, 'fast')
   });
-  
-  
-  $('#contact :first-child a').toggle(
-    function() {
-     $('#mobile-window p').animate({ left: '+=98'})
-     return false
-    },
-    function() {
-      $('#mobile-window p').animate({ left: '-=98'})
-      return false
-    });
    
-  $('#main-content .project-tab').hover(
+  $('#portfolio .project-tab').hover(
     function() {
       $(this).stop().animate({ backgroundColor: '#00b0d4'})
-      $(this).children('h2').stop().animate({color: '#fff'})
+      $(this).children('h3').stop().animate({color: '#fff'})
       $(this).children('img').stop().animate({borderColor: '#23a1bb'})
     }, 
     function() {
       $(this).stop().animate({ backgroundColor: '#f2f2f2'})
-      $(this).children('h2').stop().animate({color: '#00b0d4'})
+      $(this).children('h3').stop().animate({color: '#00b0d4'})
       $(this).children('img').stop().animate({borderColor: '#bbbbbb'})
     });
     
-    // $('#project-content .project-screenshot a').hover(
-    // function() {
-    //   $(this).parent().animate({borderColor: '#00b0d4'})
-    // },
-    // function() {
-    //   $(this).parent().animate({borderColor: '#aeaeae'})
-    // });
     
 });
 
